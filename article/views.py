b8a3dc0f26afa11755bd8e4c11cfd26061bd7e1e
fktr <1767831392@qq.com>
2016-09-06 03:01:54 +0800

    Design view function for index page and test it

--- article/views.py
+++ article/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Article,Category
 
 # Create your views here.
+class IndexView(ListView):
+    template_name = 'article/index.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        article_list=Article.objects.filter(status='p')
+        return article_list
+
+    def get_context_data(self, **kwargs):
+        kwargs['category_list']=Category.objects.all().order_by('name')
+        return super(IndexView,self).get_context_data(**kwargs)
+
+
--- blog/settings.py
+++ blog/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES=os.path.join(BASE_DIR,'/article/static')
